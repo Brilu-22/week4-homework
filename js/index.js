5ba27a49eae0ef280701e0c80241f01aa32643e7
brilu-22 <231214@virtualwindow.co.za>
2023-05-21 11:17:41 +0200

    project 101
    this is the first commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+calculate = () => {
+    let valueOne = +document.getElementById("valueOne").value 
+    let valueTwo = +document.getElementById("valueTwo").value
+
+    console.log(valueOne, valueTwo)
+}
